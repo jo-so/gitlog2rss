76567f2b48eaf1b45bf52765d8de4d4d36447e07
Jörg Sommer <joerg@jo-so.de>
2020-07-08 14:22:56 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,140 @@
+# RSS feed builder from git-log
+
+This tool reads the history from one or multiple files from git and builds an
+RSS feed. Commits they contain `no-rss` in a whole line in their description are
+excluded.
+
+# Usage
+
+``` shellsession
+% cargo run -q -- --help
+gitlog2rss 0.1.0
+Jörg Sommer <joerg@jo-so.de>
+Create an RSS feed from git log
+
+USAGE:
+    gitlog2rss [FLAGS] [OPTIONS] <PATH>... --conf <FILE>
+
+ARGS:
+    <PATH>...    Path of the source file
+
+FLAGS:
+    -d, --debug      Print debug messages
+    -h, --help       Prints help information
+    -y, --pretty     Pretty print output
+    -V, --version    Prints version information
+
+OPTIONS:
+    -c, --conf <FILE>        config file
+    -p, --prefix <PREFIX>    PREFIX gets removed from the beginning of file names
+```
+
+## Config file
+
+``` yaml
+#
+# See https://validator.w3.org/feed/docs/rss2.html for a description of
+# these fields
+
+repo: /home/joerg/website/.git
+base-url: https://jo-so.de/
+# beginning of paths that should be removed before using in URLs
+strip-prefix: src/
+ignore-files:
+  - '**/index.md'
+  - 'src/lib'
+  - '**/_*'
+
+item-title-page-new: Seite /%p erstellt
+item-title-page-removed: Seite /%p gelöscht
+item-title-page-modified: Seite /%p bearbeitet
+
+channel-title: Am Interneteingang 8
+channel-description: Webseite von Jörg Sommer
+channel-link: https://jo-so.de/
+
+language: de-de
+# TTL in minutes, units like d/days, w/weeks, M/months are possible
+# see https://docs.rs/humantime/latest/humantime/fn.parse_duration.html
+ttl: 2d
+# NOT IMPLEMENTED categories:
+# NOT IMPLEMENTED   -
+
+copyright: © 2017–2020 Jörg Sommer <joerg@jo-so.de>
+managing-editor: joerg@jo-so.de (Jörg Sommer)
+webmaster: technik@jo-so.de (Jörg Sommer)
+
+generator: gitlog2rss
+
+# when you might never publish new items
+# http://backend.userland.com/skipHoursDays
+# skip-hours: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# skip-days: []
+
+# for podcasts:
+# NOT IMPLEMENTED rating: The PICS rating for the channel.
+# NOT IMPLEMENTED image:
+# NOT IMPLEMENTED text-input:
+```
+
+## Example for a single page
+
+This examples uses the shell operator `<<<` to append the string to the config
+file and feed all to *gitlog2rss* as config.
+
+``` shellsession
+% RUST_LOG_TIMESTAMP= cargo run -q -- -c - src/2020-02/Maxima.md -dy <../website/gitlog2rss.yaml <<<'channel-title: Am Interneteingang 8 -- Mathematik mit Maxima
+channel-link: https://jo-so.de/2020-02/Maxima.html'
+[INFO  gitlog2rss] Going to read config from stdin
+[INFO  gitlog2rss] using path filter src/2020-02/Maxima.md
+[INFO  gitlog2rss] Opening git repository /home/joerg/website/.git
+[INFO  gitlog2rss] Skipping commit 395d434ffe13dad77d3058ade3be3a059bbd3a58, because of "no-rss"
+[TRACE gitlog2rss] 18aea4feebd981347236d49fa5e6c84ad634b433 Added Some("src/2020-02/Maxima.md"), Some("src/2020-02/Maxima.md")
+[DEBUG gitlog2rss] New rss item for 18aea4feebd981347236d49fa5e6c84ad634b433:src/2020-02/Maxima.md
+[TRACE gitlog2rss] a3cbc8b0b0cb23407131b0125803a242c6c50ecd Modified Some("src/2020-02/Maxima.md"), Some("src/2020-02/Maxima.md")
+[DEBUG gitlog2rss] New rss item for a3cbc8b0b0cb23407131b0125803a242c6c50ecd:src/2020-02/Maxima.md
+[INFO  gitlog2rss] Skipping commit e891e94849de9a1cadac38fbf0f88df437e0d8cd, because of "no-rss"
+[INFO  gitlog2rss] Skipping commit 9c311fd04617ce8d36824f65788b3a5c40e0eb35, because of "no-rss"
+<rss version="2.0">
+  <channel>
+    <title>Am Interneteingang 8 -- Mathematik mit Maxima</title>
+    <link>https://jo-so.de/2020-02/Maxima.html</link>
+    <description>Webseite von Jörg Sommer</description>
+    <language>de-de</language>
+    <copyright>© 2017–2020 Jörg Sommer &lt;joerg@jo-so.de&gt;</copyright>
+    <managingEditor>joerg@jo-so.de (Jörg Sommer)</managingEditor>
+    <webMaster>technik@jo-so.de (Jörg Sommer)</webMaster>
+    <pubDate>Fri, 03 Apr 2020 09:57:30 +0200</pubDate>
+    <lastBuildDate>Mon, 20 Apr 2020 12:36:16 +0200</lastBuildDate>
+    <generator>gitlog2rss</generator>
+    <ttl>2880</ttl>
+    <item>
+      <title>Seite /2020-02/Maxima.html erstellt</title>
+      <link>https://jo-so.de/2020-02/Maxima.html</link>
+      <author>joerg@jo-so.de (Jörg Sommer)</author>
+      <pubDate>Fri, 03 Apr 2020 09:57:30 +0200</pubDate>
+    </item>
+    <item>
+      <title>Seite /2020-02/Maxima.html bearbeitet</title>
+      <link>https://jo-so.de/2020-02/Maxima.html</link>
+      <author>joerg@jo-so.de (Jörg Sommer)</author>
+      <pubDate>Mon, 20 Apr 2020 12:36:16 +0200</pubDate>
+    </item>
+  </channel>
+</rss>```
+
+## Example with the whole website
+
+``` shellsession
+% /path/to/gitlog2rss/target/release/gitlog2rss \
+  -c gitlog2rss.yaml 'src/**/*.md' > www/rss
+```
+
+# Further reading
+
+* <https://validator.w3.org/feed/docs/rss2.html>
+* <https://validator.w3.org/feed/check.cgi>
+
+# TODO
+
+* add [anyhow](https://crates.io/crates/anyhow) for better error messages
---
+++ src/main.rs
@@ -0,0 +1,316 @@
+#![feature(inner_deref)]
+
+use chrono::{
+    FixedOffset,
+    TimeZone,
+};
+use clap::{
+    crate_authors,
+    crate_description,
+    crate_name,
+    crate_version,
+    App,
+    Arg,
+};
+use git2::{
+    Delta,
+    DiffFindOptions,
+    DiffOptions,
+    Pathspec,
+    PathspecFlags,
+    Repository,
+    Sort,
+};
+use log::{
+    debug,
+    info,
+    trace,
+    warn,
+};
+use rss::{
+    ChannelBuilder,
+    ItemBuilder,
+};
+use std::{
+    env,
+    error,
+    fs,
+    io::{self, Read},
+};
+use yaml_rust::{
+    Yaml,
+    YamlLoader,
+};
+
+fn rfc822_time(time: &git2::Time) -> String {
+    FixedOffset::east(time.offset_minutes() * 60)
+        .timestamp(time.seconds(), 0)
+        .to_rfc2822()
+}
+
+fn main() -> Result<(), Box<dyn error::Error + 'static>> {
+    let args = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!(", "))
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("conf")
+                .short('c')
+                .long("conf")
+                .takes_value(true)
+                .value_name("FILE")
+                .required(true)
+                .about("config file")
+        ).arg(
+            Arg::with_name("debug")
+                .short('d')
+                .long("debug")
+                .about("Print debug messages")
+        ).arg(
+            Arg::with_name("prefix")
+                .short('p')
+                .long("prefix")
+                .takes_value(true)
+                .value_name("PREFIX")
+                .about("PREFIX gets removed from the beginning of file names")
+        ).arg(
+            Arg::with_name("pretty")
+                .short('y')
+                .long("pretty")
+                .about("Pretty print output")
+        ).arg(
+            Arg::with_name("path")
+                .value_name("PATH")
+                .about("Path of the source file")
+                .required(true)
+                .multiple(true)
+        ).get_matches();
+
+    {
+        let mut logger = env_logger::builder();
+        match env::var("RUST_LOG_TIMESTAMP").as_deref() {
+            Ok("sec") => { logger.format_timestamp_secs(); }
+            Ok("micro") => { logger.format_timestamp_micros(); }
+            Ok("milli") => { logger.format_timestamp_millis(); }
+            Ok("nano") => { logger.format_timestamp_nanos(); }
+            Ok(_) => { logger.format_timestamp(None); }
+            _ => {},
+        }
+
+        if args.is_present("debug") {
+            logger.filter_level(log::LevelFilter::Trace);
+        }
+
+        logger.init();
+    }
+
+    let conf = {
+        let txt = match args.value_of("conf").unwrap() {
+            "-" => {
+                info!("Going to read config from stdin");
+                let mut buf = String::new();
+                io::stdin().read_to_string(&mut buf)?;
+                buf
+            }
+            path => {
+                info!("Going to read config file {}", path);
+                fs::read_to_string(path)?
+            }
+        };
+
+        YamlLoader::load_from_str(&txt)?.pop().unwrap()
+    };
+
+    let mut diff_opts = DiffOptions::new();
+    diff_opts.ignore_filemode(true)
+        .ignore_submodules(true)
+        .ignore_whitespace(true);
+
+    for e in args.values_of("path").unwrap() {
+        info!("using path filter {}", e);
+        diff_opts.pathspec(e);
+    }
+
+    let mut diff_similar_opts = DiffFindOptions::default();
+    diff_similar_opts.renames(true);
+
+    let ignored_files = if let Some(list) = conf["ignore-files"].as_vec() {
+        Some(Pathspec::new(list.iter().filter_map(|x| x.as_str()))?)
+    } else {
+        None
+    };
+
+    let repo = if let Some(path) = conf["repo"].as_str() {
+        info!("Opening git repository {}", path);
+        Repository::open(path)?
+    } else {
+        let repo = Repository::open_from_env()?;
+        info!("Successfully opened git repository {}", repo.path().display());
+        repo
+    };
+
+    let base_url = url::Url::parse(conf["base-url"].as_str().unwrap())?;
+    let strip_prefix = args.value_of("prefix")
+        .or_else(|| conf["strip-prefix"].as_str())
+        .unwrap_or("");
+
+    let mut items = Vec::new();
+
+    let mut revwalk = repo.revwalk()?;
+    revwalk.set_sorting(Sort::REVERSE | Sort::TIME)?;
+    revwalk.push_head()?;
+    for id in revwalk {
+        let commit = repo.find_commit(id?)?;
+        if commit.parent_count() > 1 {
+            debug!("Skipping merge commit {}", commit.id());
+            continue;
+        }
+        if commit.message().map_or(false, |msg| msg.contains("\nno-rss\n")) {
+            info!("Skipping commit {}, because of \"no-rss\"", commit.id());
+            continue;
+        }
+
+        let author = commit.author();
+        let author_date = rfc822_time(&author.when());
+        let author = author.email().unwrap().to_string()
+            + " (" + author.name().unwrap() + ")";
+
+        let parent_tree = if commit.parent_count() == 1 {
+            Some(commit.parent(0)?.tree()?)
+        } else {
+            None
+        };
+
+        let diff = repo.diff_tree_to_tree(
+            parent_tree.as_ref(), Some(&commit.tree()?), Some(&mut diff_opts)
+        )?;
+        // to find renames or copies
+        // diff.find_similar(Some(&mut diff_similar_opts))?;
+
+        for delta in diff.deltas() {
+            trace!("{} {:?} {:?}, {:?}",
+                   commit.id(),
+                   delta.status(),
+                   delta.old_file().path(),
+                   delta.new_file().path(),
+            );
+
+            let file;
+            let text;
+            match delta.status() {
+                Delta::Added => {
+                    file = delta.new_file();
+                    text = "item-title-page-new";
+                }
+
+                Delta::Deleted => {
+                    file = delta.old_file();
+                    text = "item-title-page-removed";
+                }
+
+                Delta::Modified => {
+                    file = delta.new_file();
+                    text = "item-title-page-modified"
+                }
+
+                st => {
+                    warn!(
+                        "Unhandled diff state {:?} for commit {} between {:?} and {:?}",
+                        st,
+                        commit.id(),
+                        delta.old_file().path(),
+                        delta.new_file().path(),
+                    );
+                    continue;
+                }
+            }
+
+            let path = file.path().unwrap();
+
+            if let Some(ref ign) = ignored_files {
+                if ign.matches_path(&path, PathspecFlags::default()) {
+                    info!("Skipping delta of ignored file {} in commit {}",
+                          path.display(), commit.id());
+                    continue;
+                }
+            }
+
+            let path = path.to_str().unwrap();
+            let url_path = {
+                let first = if path.starts_with(strip_prefix) { strip_prefix.len() } else { 0 };
+
+                if path.ends_with(".md") {
+                    path[first..path.len() - 2].to_string() + "html"
+                } else {
+                    path[first..].to_string()
+                }
+            };
+
+            items.push(
+                ItemBuilder::default()
+                    .author(Some(author.clone()))
+                // TODO .description(Some("Neue Seite erstellt".into()));
+                // TODO .categories(vec![])
+                // TODO .guid(Some(Guid))
+                    .pub_date(Some(author_date.clone()))
+                    .title(
+                        conf[text].as_str().map(|title| title.replace("%p", &url_path))
+                    )
+                    .link(Some(base_url.join(&url_path)?.into_string()))
+                    .build().unwrap()
+            );
+            debug!("New rss item for {}:{}", commit.id(), path)
+        }
+    }
+
+    let chan = ChannelBuilder::default()
+        .title(conf["channel-title"].as_str().unwrap())
+        .link(conf["channel-link"].as_str().unwrap())
+        .description(conf["channel-description"].as_str().unwrap())
+        .pub_date(items.first().and_then(|x| x.pub_date()).map(|x| x.to_owned()))
+        .last_build_date(items.last().and_then(|x| x.pub_date()).map(|x| x.to_owned()))
+        .language(conf["language"].as_str().map(|x| x.to_owned()))
+        .copyright(conf["copyright"].as_str().map(|x| x.to_owned()))
+        .managing_editor(conf["managing-editor"].as_str().map(|x| x.to_owned()))
+        .webmaster(conf["webmaster"].as_str().map(|x| x.to_owned()))
+    // TODO .categories(vec![])
+        .generator(conf["generator"].as_str().map(|x| x.to_owned()))
+        .ttl(match &conf["ttl"] {
+            Yaml::Integer(x) => Some(format!("{}", x)),
+            Yaml::String(x) => Some(format!("{}", humantime::parse_duration(x)?.as_secs() / 60)),
+            Yaml::BadValue => None,
+            _ => return Err("Invalid value of config entry 'ttl'".into())
+        })
+        .skip_hours(
+            conf["skip-hours"].as_vec()
+                .map_or(
+                    vec![],
+                    |vec| vec.iter()
+                        .filter_map(|x| x.as_i64())
+                        .map(|x| format!("{}", x))
+                        .collect()
+                )
+        )
+        .skip_days(
+            conf["skip-days"].as_vec()
+                .map_or(
+                    vec![],
+                    |vec| vec.iter()
+                        .filter_map(|x| x.as_i64())
+                        .map(|x| format!("{}", x))
+                        .collect()
+                )
+        )
+        .items(items)
+        .build()
+        .unwrap();
+
+    if args.is_present("pretty") {
+        chan.pretty_write_to(&mut io::stdout(), b' ', 2)?;
+        println!("");
+    } else {
+        chan.write_to(&mut io::stdout())?;
+    }
+
+    Ok(())
+}
